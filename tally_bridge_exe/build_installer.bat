a8b63154990b87178da6c9794169f269fc0e2b14
advsivasankarank <advsivasankaran.k@gmail.com>
2026-04-13 10:24:19 +0530

    Improve installer build script

--- tally_bridge_exe/build_installer.bat
+++ tally_bridge_exe/build_installer.bat
@@ -8,7 +8,18 @@ if not exist "dist\ebal-tally-bridge.exe" (
 )
 
 echo Building installer (requires Inno Setup)...
-iscc installer.iss
+
+set ISCC_PATH=
+if exist "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" set ISCC_PATH=C:\Program Files (x86)\Inno Setup 6\ISCC.exe
+if exist "C:\Program Files\Inno Setup 6\ISCC.exe" set ISCC_PATH=C:\Program Files\Inno Setup 6\ISCC.exe
+
+if "%ISCC_PATH%"=="" (
+  echo Inno Setup not found. Please install Inno Setup 6.
+  pause
+  exit /b 1
+)
+
+"%ISCC_PATH%" installer.iss
 
 echo Done. Installer is in dist\ebal-tally-bridge-setup.exe
 pause
