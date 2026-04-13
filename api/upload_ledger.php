847f50880f390d8949131bcaf8afb028dee16e54
advsivasankarank <advsivasankaran.k@gmail.com>
2026-04-13 22:24:04 +0530

    Fix API include paths for public folder

--- api/upload_ledger.php
+++ api/upload_ledger.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once __DIR__ . '/../config/database.php';
-require_once __DIR__ . '/../app/helpers/xml_sanitizer.php';
-require_once __DIR__ . '/../config/app.php';
+require_once __DIR__ . '/../../config/database.php';
+require_once __DIR__ . '/../../app/helpers/xml_sanitizer.php';
+require_once __DIR__ . '/../../config/app.php';
 
 header('Content-Type: application/json; charset=utf-8');
 
--- api/upload_tb.php
+++ api/upload_tb.php
@@ -1,9 +1,9 @@
 <?php
 
-require_once __DIR__ . '/../config/database.php';
-require_once __DIR__ . '/../app/helpers/xml_sanitizer.php';
-require_once __DIR__ . '/../app/helpers/tb_import_helper.php';
-require_once __DIR__ . '/../config/app.php';
+require_once __DIR__ . '/../../config/database.php';
+require_once __DIR__ . '/../../app/helpers/xml_sanitizer.php';
+require_once __DIR__ . '/../../app/helpers/tb_import_helper.php';
+require_once __DIR__ . '/../../config/app.php';
 
 header('Content-Type: application/json; charset=utf-8');
 
